0f51917e3e035aeea92b0446c45ca25b7a8969b2
Mpr5377 <35986674+Mpr5377@users.noreply.github.com>
2019-12-02 20:36:03 -0500

    Added Calculator_Logic.py
    Initial code commit, pretty bare bones

---
+++ Calculator_Logic.py
@@ -0,0 +1,41 @@
+"""
+File Name: Calculator_Logic.py
+Name: Matt Robinson
+Description: This is just going to be a small program to test the logic for calculating GPA
+"""
+
+
+grades = {
+    "A+": 4.0,
+    "A": 4.0,
+    "A-": 3.7,
+    "B+": 3.3,
+    "B": 3.0,
+    "B-": 2.7,
+    "C+": 2.3,
+    "C": 2.0,
+    "C-": 1.7,
+    "D+": 1.3,
+    "D": 1,
+    "E": 0,
+    "F": 0
+}
+
+
+def calculate_gpa():
+    """
+    Function: calculate_gpa()
+    :param
+    :return GPA Value on a 4.0 Scale
+    Description: Does the following Math to return the GPA value on a 4.0 Scale:
+
+    GPA MATH:
+        SUMMATION of (Grade * Credit Hours)
+        ----------------------------------- = GPA
+                   Credit Hours
+    """
+    pass
+
+
+if __name__ == '__main__':
+    calculate_gpa()
