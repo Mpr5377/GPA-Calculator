0bf2c5f3460d545aed5473f070cca3ac6d02f39f
Mpr5377 <35986674+Mpr5377@users.noreply.github.com>
2019-12-06 17:30:47 -0500

    Click button to add element to screen

--- Tkinter_Experiments/Tkinter_Practice.py
+++ Tkinter_Experiments/Tkinter_Practice.py
@@ -8,16 +8,20 @@ from tkinter import *
 
 
 def test():
+    buttons = int(input("How many buttons?"))
     m = Tk()
     m.title('GPA Calculator')
-    m.wm_minsize(width=1000,height=500)
-    button = Button(m, text="Test", width=25, command=lambda: button_function("test"))
-    button.pack()
+    m.wm_minsize(width=1000, height=500)
+    for i in range(0, buttons):
+        button = Button(m, text="Test", height=1, width=25, command=lambda: button_function("test", m))
+        button.place(x=0, y=i*100)
     m.mainloop()
 
 
-def button_function(text):
+def button_function(text,m):
     print(text)
+    button = Button(m, text="who knows", height=1, width=25, command=lambda: print("blah"))
+    button.place(relx=.5, rely=.5)
 
 
 if __name__ == '__main__':
