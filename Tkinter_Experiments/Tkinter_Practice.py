4c3effcd21ce3e9a41b823f0ae8754426ab812ef
Mpr5377 <35986674+Mpr5377@users.noreply.github.com>
2019-12-06 18:13:25 -0500

    Figured out header for GUI

--- Tkinter_Experiments/Tkinter_Practice.py
+++ Tkinter_Experiments/Tkinter_Practice.py
@@ -7,6 +7,14 @@ Description: This file's sole purpose is to just get me familiar with Tkinter so
 from tkinter import *
 
 
+'''
+Useful Links:
+
+https://stackoverflow.com/questions/36575890/how-to-set-a-tkinter-window-to-a-constant-size
+https://www.geeksforgeeks.org/python-gui-tkinter/
+'''
+
+
 def test():
     buttons = int(input("How many buttons?"))
     m = Tk()
@@ -24,5 +32,15 @@ def button_function(text,m):
     button.place(relx=.5, rely=.5)
 
 
+def pretty_layout():
+    m = Tk()
+    m.title('GPA Calculator')
+    m.wm_minsize(width=1000, height=500)
+    title_bar1 = Label(m, text="Class Name", width=24, font=("Times New Roman", 30)).grid(row=0, column=0)
+    title_bar2 = Label(m, text="Credit Hours", width=25, font=("Times New Roman", 30)).grid(row=0, column=1)
+    title_bar3 = Label(m, text="Grade", width=25, font=("Times New Roman", 30)).grid(row=0, column=2)
+    m.mainloop()
+
+
 if __name__ == '__main__':
-    test()
+    pretty_layout()
